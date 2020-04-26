62721004998b7d54f9e9effa33b3627d7ad6317f
Eoin O'Neill <eoinoneill1991@gmail.com>
2020-04-26 16:54:39 -0700

    Almost working GGPO impelmentation.
    There are design issues to be resolved, save/load state isn't working
    properly, etc etc.

--- scene/main/scene_tree_lockstep.h
+++ scene/main/scene_tree_lockstep.h
@@ -8,33 +8,46 @@
 
 struct GGPOInput {
     uint32_t buttons = 0;
-};
 
-struct GGPOState {
-    struct PlayerState {
-        int x;
-        int y;
-    };
-
-    PlayerState player_state[2] = {{0, 0},
-                                   {0, 0}};
+    static GGPOInput random(){
+        GGPOInput to_return = GGPOInput{};
+        to_return.buttons = rand();
+        return to_return;
+    }
 };
 
 class GGPONetwork : public Object {
     GDCLASS(GGPONetwork, Object)
 
 private:
+    struct GGPOSessionDetails {
+        GGPOSessionDetails(uint8_t max_players, bool sync_test = false)
+           : sync_testing(sync_test),
+             max_player_count(max_players){
+        }
+
+        const bool sync_testing;
+
+        const uint8_t max_player_count;
+        uint8_t current_player_count;
+        uint8_t remote_player_count;
+
+        ~GGPOSessionDetails(){
+        }
+
+    };
+
     static GGPONetwork* instance;
     class SceneTreeLockstep* tree;
 
     GGPOSession* session = nullptr;
+    GGPOSessionDetails* details = nullptr;
+
     GGPOPlayer players[8];
     GGPOPlayerHandle handles[8];
-
-    uint8_t total_player_count = 0;
-    uint8_t current_player_count = 0;
-    uint8_t remote_player_count = 0;
     uint16_t port = 0;
+
+
 
     void set_tree(SceneTreeLockstep* tree) { this->tree = tree; }
 
@@ -48,6 +61,7 @@ public:
 
     //These functions need to be defined, should wrap around GGPOs concept of a session.
     bool start_session(Object *tree, String name, uint16_t num_players, uint16_t udp_port);
+    bool start_sync_test(Object* tree_object, String name, uint16_t num_players);
     bool add_player(PlayerType type, String ip_address = "");
     bool add_local_input(uint8_t index, GGPOInput* input);
     bool synchronize_inputs(GGPOInput* inputs);
@@ -56,14 +70,15 @@ public:
     void close_session();
 
     SceneTreeLockstep* get_tree();
-    uint8_t get_current_player_count() { return current_player_count; }
+    uint8_t get_current_player_count() { return details ? details->current_player_count : 0; }
     bool is_player_local(uint8_t id);
-    bool has_remote_players() { return remote_player_count > 0; }
+    bool has_remote_players() { return details ? details->remote_player_count > 0 : false; }
     bool is_game_in_session();
+    bool is_sync_testing();
 
     static bool cb_begin_game(const char *game);
 
-    static bool cb_advance_frame(int);
+    static bool cb_advance_frame(int catch_up);
 
     static bool cb_save_game_state(unsigned char **out_buffer, int *out_len,
             int *out_checksum, int frame);
@@ -88,11 +103,11 @@ private:
     float fixed_delta;
 
 public:
-    GGPOState game_state = GGPOState(); // placeholder state for serialization
+    Dictionary game_state;
 
     bool iteration(float p_time) override;
     bool idle(float p_time) override;
-    bool advance_frame(int);
+    bool advance_frame(int flags);
     void request_inputs_for_local_player(GGPOInput &input, uint8_t player_id);
     void lockstep_update(GGPOInput* inputs, uint8_t num_players, float p_time);
     void write_gamestate();
@@ -101,14 +116,11 @@ public:
 
     Object* get_ggpo() { return GGPONetwork::get_singleton(); }
 
-    GGPOState* get_game_state_ptr() { return &game_state; }
-    void set_gamestate(Dictionary dict);
+    void load_game_state(Dictionary dict);
 
     Dictionary serialize_ggpo_input(GGPOInput& input);
     GGPOInput deserialize_ggpo_input(Dictionary dictionary);
 
-    Dictionary serialize_ggpo_gamestate(GGPOState& state);
-    GGPOState deserialize_ggpo_gamestate(Dictionary dictionary);
 };
 
 #endif // SCENE_TREE_LOCKSTEP_H
