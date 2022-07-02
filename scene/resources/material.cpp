97ad0a11d11db67cfb106723c213c29e53a5090a
Emmet O'Neill <emmetoneill.pdx@gmail.com>
2022-07-02 14:37:49 -0700

    Changed Sprite Material to BLEND_MODE_ADD for use with label3d.

--- scene/resources/material.cpp
+++ scene/resources/material.cpp
@@ -1785,6 +1785,7 @@ RID SpatialMaterial::get_material_rid_for_2d(bool p_shaded, bool p_transparent, 
 		material->set_flag(FLAG_BILLBOARD_KEEP_SCALE, true);
 		material->set_billboard_mode(p_billboard_y ? BILLBOARD_FIXED_Y : BILLBOARD_ENABLED);
 	}
+    material->set_blend_mode(BLEND_MODE_ADD);
 
 	materials_for_2d[hash] = material;
 	// flush before using so we can access the shader right away
